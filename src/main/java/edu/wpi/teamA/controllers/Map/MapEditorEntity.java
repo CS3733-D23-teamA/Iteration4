8ba917ebd5916448c71d5c03d102e89f3805e4dc
Seth Frank <sbfrank@wpi.edu>
2023-04-09 15:56:11 -0400

    Display nodes for all floors on map editor and switch between floors

--- src/main/java/edu/wpi/teamA/controllers/Map/MapEditorEntity.java
+++ src/main/java/edu/wpi/teamA/controllers/Map/MapEditorEntity.java
@@ -1,3 +1,18 @@
 package edu.wpi.teamA.controllers.Map;
 
-public class MapEditorEntity {}
+import edu.wpi.teamA.database.ORMclasses.Node;
+import java.util.ArrayList;
+
+public class MapEditorEntity {
+  // MapEditorController controller = new MapEditorController();
+
+  public ArrayList<Node> getFloorNodes(ArrayList<Node> nodeArray, String floor) {
+    ArrayList<Node> updatedArray = new ArrayList<Node>();
+    for (Node node : nodeArray) {
+      if (node.getFloor().equals(floor)) {
+        updatedArray.add(node);
+      }
+    }
+    return updatedArray;
+  }
+}
--- src/main/java/edu/wpi/teamA/controllers/Navigation/MapEditorController.java
+++ src/main/java/edu/wpi/teamA/controllers/Navigation/MapEditorController.java
@@ -1,23 +1,45 @@
 package edu.wpi.teamA.controllers.Navigation;
 
 import edu.wpi.teamA.App;
+import edu.wpi.teamA.Main;
+import edu.wpi.teamA.controllers.Map.MapEditorEntity;
 import edu.wpi.teamA.database.DAOImps.NodeDAOImp;
 import edu.wpi.teamA.database.ORMclasses.Node;
+import io.github.palexdev.materialfx.controls.MFXButton;
 import java.util.ArrayList;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import net.kurobako.gesturefx.GesturePane;
 
 public class MapEditorController {
+  MapEditorEntity entity = new MapEditorEntity();
   private NodeDAOImp nodeDAO = new NodeDAOImp();
+  private ArrayList<Node> levelGNodeArray;
+  private ArrayList<Node> levelL1NodeArray;
+  private ArrayList<Node> levelL2NodeArray;
+  private ArrayList<Node> level1NodeArray;
+  private ArrayList<Node> level2NodeArray;
+  private ArrayList<Node> level3NodeArray;
 
   @FXML private ImageView mapImage;
   @FXML private StackPane mapStackPane;
   @FXML private AnchorPane dotsAnchorPane;
   @FXML private GesturePane mapGesturePane;
+
+  @FXML private MFXButton levelButton;
+  @FXML private MFXButton levelGButton;
+  @FXML private MFXButton levelL1Button;
+  @FXML private MFXButton levelL2Button;
+  @FXML private MFXButton level1Button;
+  @FXML private MFXButton level2Button;
+  @FXML private MFXButton level3Button;
+
+  @FXML private VBox levelMenu;
 
   public void initialize() {
     double xCoord = mapImage.getBoundsInParent().getMinX();
@@ -29,46 +51,89 @@ public class MapEditorController {
     System.out.println(width);
     System.out.println(height);
     mapGesturePane.setContent(mapStackPane);
-    displayNodeData(nodeDAO.loadNodesFromDatabase());
+    // displayNodeData(nodeDAO.loadNodesFromDatabase());
+
+    levelMenu.setVisible(false);
+
+    levelGButton.setOnAction(event -> changeLevelText(levelGButton));
+    levelL1Button.setOnAction(event -> changeLevelText(levelL1Button));
+    levelL2Button.setOnAction(event -> changeLevelText(levelL2Button));
+    level1Button.setOnAction(event -> changeLevelText(level1Button));
+    level2Button.setOnAction(event -> changeLevelText(level2Button));
+    level3Button.setOnAction(event -> changeLevelText(level3Button));
+
+    ArrayList<Node> nodeArray = nodeDAO.loadNodesFromDatabase();
+    // get node arrays
+    levelGNodeArray = entity.getFloorNodes(nodeArray, "G");
+    levelL1NodeArray = entity.getFloorNodes(nodeArray, "L1");
+    levelL2NodeArray = entity.getFloorNodes(nodeArray, "L2");
+    level1NodeArray = entity.getFloorNodes(nodeArray, "1");
+    level2NodeArray = entity.getFloorNodes(nodeArray, "2");
+    level3NodeArray = entity.getFloorNodes(nodeArray, "3");
+
+    changeLevelText(levelGButton);
   }
 
-  public void displayNodeData(ArrayList<Node> nodeArray) {
-    ArrayList<Node> updatedArray = new ArrayList<Node>();
-    ArrayList<Circle> dotsToBeDisplayed = new ArrayList<Circle>();
+  @FXML
+  public void levelChooser() {
+    // make Vbox visible
+    levelMenu.setVisible(true);
+  }
+
+  public void changeLevelText(MFXButton button) {
+
+    // get text from clicked button
+    levelButton.setText(button.getText());
+    // System.out.println(button.getText());
+
+    // hide buttons in Vbox
+    levelMenu.setVisible(false);
+
+    String image = "images/map-page/" + button.getText() + ".png";
+
+    // change map image
+    mapImage.setImage(new Image(Main.class.getResource(image).toString()));
+
+    // hide old dots
+    dotsAnchorPane.getChildren().clear();
+
+    // display new dots
+    displayNodeData(determineArray(button.getText()));
+  }
+
+  private ArrayList<Node> determineArray(String level) {
+    if (level.equals("Level G")) {
+      return levelGNodeArray;
+    } else if (level.equals("Level L1")) {
+      return levelL1NodeArray;
+    } else if (level.equals("Level L2")) {
+      return levelL2NodeArray;
+    } else if (level.equals("Level 1")) {
+      return level1NodeArray;
+    } else if (level.equals("Level 2")) {
+      return level2NodeArray;
+    } else if (level.equals("Level 3")) {
+      return level3NodeArray;
+    } else {
+      return null;
+    }
+  }
+
+  public void displayNodeData(ArrayList<Node> nodeArrayForFloor) {
     double scalar = 0.144;
-    for (int i = 0; i < nodeArray.size(); i++) {
-      Node node = nodeArray.get(i);
-      if (nodeArray.get(i).getFloor().equals("L1")) {
-        int originalNodeX = node.getXcoord();
-        int originalNodeY = node.getYcoord();
-        double newXCoord = originalNodeX * scalar;
-        double newYCoord = originalNodeY * scalar;
+    for (Node node : nodeArrayForFloor) {
+      int originalNodeX = node.getXcoord();
+      int originalNodeY = node.getYcoord();
+      double newXCoord = originalNodeX * scalar;
+      double newYCoord = originalNodeY * scalar;
 
-        // System.out.println(newXCoord);
-        // System.out.println(newYCoord);
-
-        /// FUNCTION to add circle
-        Circle circle = addCircle(newXCoord, newYCoord);
-        dotsToBeDisplayed.add(circle);
-        System.out.println(circle.getCenterX());
-        System.out.println(circle.getCenterY());
-        dotsAnchorPane.getChildren().add(circle);
-        // circle.setCenterX(newXCoord);
-        // circle.setCenterY(newYCoord);
-        // circle.setRadius(2);
-        // mapStackPane.getChildren().add(addCircle(newXCoord, newYCoord));
-
-        updatedArray.add(nodeArray.get(i));
-      }
+      Circle circle = addCircle(newXCoord, newYCoord);
+      System.out.println(circle.getCenterX());
+      System.out.println(circle.getCenterY());
+      dotsAnchorPane.getChildren().add(circle);
     }
 
     App.getPrimaryStage().show();
-    /*ObservableList<javafx.scene.Node> allDots = mapStackPane.getChildren();
-    allDots = FXCollections.observableArrayList(dotsToBeDisplayed);
-    System.out.println(allDots.size());
-    mapStackPane.getChildren().add()
-    App.getPrimaryStage().getScene();*/
-    // App.getPrimaryStage().show();
   }
 
   public Circle addCircle(double X, double Y) {
