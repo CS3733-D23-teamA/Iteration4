b7c0de35796c8269c3daebc329077ed0cd199c4e
Seth Frank <sbfrank@wpi.edu>
2023-04-09 20:19:45 -0400

    Slight restructuring of map editor

--- src/main/java/edu/wpi/teamA/controllers/Map/MapEditorEntity.java
+++ src/main/java/edu/wpi/teamA/controllers/Map/MapEditorEntity.java
@@ -1,10 +1,20 @@
 package edu.wpi.teamA.controllers.Map;
 
+import edu.wpi.teamA.database.DAOImps.NodeDAOImp;
 import edu.wpi.teamA.database.ORMclasses.Node;
 import java.util.ArrayList;
+import javafx.scene.shape.Circle;
 
 public class MapEditorEntity {
   // MapEditorController controller = new MapEditorController();
+  private final NodeDAOImp nodeDAO = new NodeDAOImp();
+  ArrayList<Node> nodeArray = nodeDAO.loadNodesFromDatabase();
+  ArrayList<Node> levelGNodeArray = getFloorNodes(nodeArray, "G");
+  ArrayList<Node> levelL1NodeArray = getFloorNodes(nodeArray, "L1");
+  ArrayList<Node> levelL2NodeArray = getFloorNodes(nodeArray, "L2");
+  ArrayList<Node> level1NodeArray = getFloorNodes(nodeArray, "1");
+  ArrayList<Node> level2NodeArray = getFloorNodes(nodeArray, "2");
+  ArrayList<Node> level3NodeArray = getFloorNodes(nodeArray, "3");
 
   public ArrayList<Node> getFloorNodes(ArrayList<Node> nodeArray, String floor) {
     ArrayList<Node> updatedArray = new ArrayList<Node>();
@@ -14,5 +24,31 @@ public class MapEditorEntity {
       }
     }
     return updatedArray;
+  }
+
+  public ArrayList<Node> determineArray(String level) {
+    if (level.equals("Level G")) {
+      return levelGNodeArray;
+    } else if (level.equals("Level L1")) {
+      return levelL1NodeArray;
+    } else if (level.equals("Level L2")) {
+      return levelL2NodeArray;
+    } else if (level.equals("Level 1")) {
+      return level1NodeArray;
+    } else if (level.equals("Level 2")) {
+      return level2NodeArray;
+    } else if (level.equals("Level 3")) {
+      return level3NodeArray;
+    } else {
+      return null;
+    }
+  }
+
+  public Circle addCircle(double X, double Y) {
+    Circle circle = new Circle();
+    circle.setCenterX(X);
+    circle.setCenterY(Y);
+    circle.setRadius(2);
+    return circle;
   }
 }
--- src/main/java/edu/wpi/teamA/controllers/Navigation/MapEditorController.java
+++ src/main/java/edu/wpi/teamA/controllers/Navigation/MapEditorController.java
@@ -3,10 +3,10 @@ package edu.wpi.teamA.controllers.Navigation;
 import edu.wpi.teamA.App;
 import edu.wpi.teamA.Main;
 import edu.wpi.teamA.controllers.Map.MapEditorEntity;
-import edu.wpi.teamA.database.DAOImps.NodeDAOImp;
 import edu.wpi.teamA.database.ORMclasses.Node;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import java.util.ArrayList;
+import java.util.Objects;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -18,13 +18,6 @@ import net.kurobako.gesturefx.GesturePane;
 
 public class MapEditorController {
   MapEditorEntity entity = new MapEditorEntity();
-  private NodeDAOImp nodeDAO = new NodeDAOImp();
-  private ArrayList<Node> levelGNodeArray;
-  private ArrayList<Node> levelL1NodeArray;
-  private ArrayList<Node> levelL2NodeArray;
-  private ArrayList<Node> level1NodeArray;
-  private ArrayList<Node> level2NodeArray;
-  private ArrayList<Node> level3NodeArray;
 
   @FXML private ImageView mapImage;
   @FXML private StackPane mapStackPane;
@@ -42,18 +35,14 @@ public class MapEditorController {
   @FXML private VBox levelMenu;
 
   public void initialize() {
-    double xCoord = mapImage.getBoundsInParent().getMinX();
-    double yCoord = mapImage.getBoundsInParent().getMinY();
-    double width = mapImage.getBoundsInParent().getWidth();
-    double height = mapImage.getBoundsInParent().getHeight();
-    System.out.println(xCoord);
-    System.out.println(yCoord);
-    System.out.println(width);
-    System.out.println(height);
-    mapGesturePane.setContent(mapStackPane);
-    // displayNodeData(nodeDAO.loadNodesFromDatabase());
-
-    levelMenu.setVisible(false);
+    //    double xCoord = mapImage.getBoundsInParent().getMinX();
+    //    double yCoord = mapImage.getBoundsInParent().getMinY();
+    //    double width = mapImage.getBoundsInParent().getWidth();
+    //    double height = mapImage.getBoundsInParent().getHeight();
+    //    System.out.println(xCoord);
+    //    System.out.println(yCoord);
+    //    System.out.println(width);
+    //    System.out.println(height);
 
     levelGButton.setOnAction(event -> changeLevelText(levelGButton));
     levelL1Button.setOnAction(event -> changeLevelText(levelL1Button));
@@ -62,64 +51,44 @@ public class MapEditorController {
     level2Button.setOnAction(event -> changeLevelText(level2Button));
     level3Button.setOnAction(event -> changeLevelText(level3Button));
 
-    ArrayList<Node> nodeArray = nodeDAO.loadNodesFromDatabase();
-    // get node arrays
-    levelGNodeArray = entity.getFloorNodes(nodeArray, "G");
-    levelL1NodeArray = entity.getFloorNodes(nodeArray, "L1");
-    levelL2NodeArray = entity.getFloorNodes(nodeArray, "L2");
-    level1NodeArray = entity.getFloorNodes(nodeArray, "1");
-    level2NodeArray = entity.getFloorNodes(nodeArray, "2");
-    level3NodeArray = entity.getFloorNodes(nodeArray, "3");
-
+    // set up page
+    mapGesturePane.setContent(mapStackPane);
+    levelMenu.setVisible(false);
     changeLevelText(levelGButton);
   }
 
+  /** for level chooser button */
   @FXML
   public void levelChooser() {
     // make Vbox visible
     levelMenu.setVisible(true);
   }
 
-  public void changeLevelText(MFXButton button) {
+  /**
+   * Used to change the level chooser text based on which floor
+   *
+   * @param button button chosen
+   */
+  private void changeLevelText(MFXButton button) {
 
     // get text from clicked button
     levelButton.setText(button.getText());
-    // System.out.println(button.getText());
 
     // hide buttons in Vbox
     levelMenu.setVisible(false);
 
-    String image = "images/map-page/" + button.getText() + ".png";
-
     // change map image
-    mapImage.setImage(new Image(Main.class.getResource(image).toString()));
+    String image = "images/map-page/" + button.getText() + ".png";
+    mapImage.setImage(new Image(Objects.requireNonNull(Main.class.getResource(image)).toString()));
 
     // hide old dots
     dotsAnchorPane.getChildren().clear();
 
     // display new dots
-    displayNodeData(determineArray(button.getText()));
+    displayNodeData(Objects.requireNonNull(entity.determineArray(button.getText())));
   }
 
-  private ArrayList<Node> determineArray(String level) {
-    if (level.equals("Level G")) {
-      return levelGNodeArray;
-    } else if (level.equals("Level L1")) {
-      return levelL1NodeArray;
-    } else if (level.equals("Level L2")) {
-      return levelL2NodeArray;
-    } else if (level.equals("Level 1")) {
-      return level1NodeArray;
-    } else if (level.equals("Level 2")) {
-      return level2NodeArray;
-    } else if (level.equals("Level 3")) {
-      return level3NodeArray;
-    } else {
-      return null;
-    }
-  }
-
-  public void displayNodeData(ArrayList<Node> nodeArrayForFloor) {
+  private void displayNodeData(ArrayList<Node> nodeArrayForFloor) {
     double scalar = 0.144;
     for (Node node : nodeArrayForFloor) {
       int originalNodeX = node.getXcoord();
@@ -127,21 +96,11 @@ public class MapEditorController {
       double newXCoord = originalNodeX * scalar;
       double newYCoord = originalNodeY * scalar;
 
-      Circle circle = addCircle(newXCoord, newYCoord);
-      System.out.println(circle.getCenterX());
-      System.out.println(circle.getCenterY());
+      Circle circle = entity.addCircle(newXCoord, newYCoord);
       dotsAnchorPane.getChildren().add(circle);
     }
 
     App.getPrimaryStage().show();
-  }
-
-  public Circle addCircle(double X, double Y) {
-    Circle circle = new Circle();
-    circle.setCenterX(X);
-    circle.setCenterY(Y);
-    circle.setRadius(2);
-    return circle;
   }
 
   @FXML
