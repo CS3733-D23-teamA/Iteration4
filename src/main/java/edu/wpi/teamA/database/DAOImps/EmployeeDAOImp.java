9a62776dff529f0635d184488d72e1de08f87b3b
pkawatkar <pkawatkar@yahoo.com>
2023-04-18 19:09:01 -0400

    refactoring Employee DAO classes fully

--- src/main/java/edu/wpi/teamA/database/DAOImps/EmployeeDAOImp.java
+++ src/main/java/edu/wpi/teamA/database/DAOImps/EmployeeDAOImp.java
@@ -4,11 +4,8 @@ import edu.wpi.teamA.database.Connection.DBConnectionProvider;
 import edu.wpi.teamA.database.Interfaces.IEmployeeDAO;
 import edu.wpi.teamA.database.ORMclasses.Employee;
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-
-import edu.wpi.teamA.database.ORMclasses.FlowerEntity;
+import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,52 +15,48 @@ public class EmployeeDAOImp implements IEmployeeDAO {
   static DBConnectionProvider employeeProvider = new DBConnectionProvider();
 
   public EmployeeDAOImp() {
-    //this.employeeMap = loadEdgesFromDatabaseInMap();
+    // this.employeeMap = loadEdgesFromDatabaseInMap();
   }
 
   public EmployeeDAOImp(HashMap<String, Employee> employeeMap) {
     this.employeeMap = employeeMap;
   }
 
-//  @Getter @Setter private ArrayList<Employee> employeeArray;
-//  static DBConnectionProvider employeeProvider = new DBConnectionProvider();
-//
-//  public EmployeeDAOImp() {
-//    this.employeeArray = new ArrayList<>();
-//  }
-//
-//  public EmployeeDAOImp(ArrayList<Employee> employeeArray) {
-//    this.employeeArray = employeeArray;
-//  }
-
+  //  @Getter @Setter private ArrayList<Employee> employeeArray;
+  //  static DBConnectionProvider employeeProvider = new DBConnectionProvider();
+  //
+  //  public EmployeeDAOImp() {
+  //    this.employeeArray = new ArrayList<>();
+  //  }
+  //
+  //  public EmployeeDAOImp(ArrayList<Employee> employeeArray) {
+  //    this.employeeArray = employeeArray;
+  //  }
 
   @Override
   public Employee getEmployee(String username) {
     return employeeMap.get(username);
   }
-  @Override
-  public HashMap<String, Employee> getAllEmployees() {
-    ArrayList<Employee> tempList = new ArrayList<>();
+
+  public HashMap<String, Employee> loadEmployeesFromDatabaseInMap() {
     try {
-      Statement stmt = employeeProvider.createConnection().createStatement();
-      ResultSet rs = stmt.executeQuery("SELECT * FROM \"Prototype2_schema\".\"Employee\"");
+      Statement st =
+          Objects.requireNonNull(DBConnectionProvider.createConnection()).createStatement();
+      ResultSet rs = st.executeQuery("SELECT * FROM \"Prototype2_schema\".\"Employee\"");
 
       while (rs.next()) {
-        String name = rs.getString("name");
+        String name = rs.getString("namee");
         String username = rs.getString("username");
         String password = rs.getString("password");
 
-        Employee temp = new Employee();
-        temp.setName(name);
-        temp.setUsername(username);
-        temp.setPassword(password);
-
-        tempList.add(temp);
+        Employee employee = new Employee(name, username, password);
+        employeeMap.put(username, employee);
       }
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
-    return tempList;
+
+    return employeeMap;
   }
 
   @Override
@@ -82,10 +75,7 @@ public class EmployeeDAOImp implements IEmployeeDAO {
       ps.setString(2, password);
       ps.setString(3, username);
 
-      employeeMap.put(
-              employee.getUsername(),
-              new Employee(
-                      name, employee.getUsername(), password));
+      employeeMap.put(employee.getUsername(), new Employee(name, employee.getUsername(), password));
 
     } catch (SQLException e) {
       throw new RuntimeException(e);
@@ -100,27 +90,23 @@ public class EmployeeDAOImp implements IEmployeeDAO {
       String password = employee.getPassword();
 
       String sqlCreateEdge =
-              "Create Table if not exists \"Prototype2_schema\".\"Employee\""
-                      + "(namee     Varchar(600),"
-                      + "username    Varchar(600),"
-                      + "password    VarChar(600),)";
+          "Create Table if not exists \"Prototype2_schema\".\"Employee\""
+              + "(namee     Varchar(600),"
+              + "username    Varchar(600),"
+              + "password    VarChar(600),)";
       Statement stmtEmployee = employeeProvider.createConnection().createStatement();
       stmtEmployee.execute(sqlCreateEdge);
 
       PreparedStatement ps =
-              employeeProvider
-                      .createConnection()
-                      .prepareStatement(
-                              "INSERT INTO \"Prototype2_schema\".\"Employee\" VALUES (?, ?, ?)");
+          employeeProvider
+              .createConnection()
+              .prepareStatement("INSERT INTO \"Prototype2_schema\".\"Employee\" VALUES (?, ?, ?)");
       ps.setString(1, name);
       ps.setString(2, username);
       ps.setString(3, password);
       ps.executeUpdate();
 
-      employeeMap.put(
-              employee.getUsername(),
-              new Employee(
-                      name, employee.getUsername(), password));
+      employeeMap.put(employee.getUsername(), new Employee(name, employee.getUsername(), password));
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
@@ -130,9 +116,10 @@ public class EmployeeDAOImp implements IEmployeeDAO {
   public void removeEmployee(Employee employee) {
     try {
       PreparedStatement ps =
-              employeeProvider
-                      .createConnection()
-                      .prepareStatement("DELETE FROM \"Prototype2_schema\".\"Employee\" WHERE username = ?");
+          employeeProvider
+              .createConnection()
+              .prepareStatement(
+                  "DELETE FROM \"Prototype2_schema\".\"Employee\" WHERE username = ?");
       ps.setString(1, employee.getUsername());
       ps.executeUpdate();
 
@@ -143,6 +130,4 @@ public class EmployeeDAOImp implements IEmployeeDAO {
       throw new RuntimeException(e);
     }
   }
-
-
 }
--- src/main/java/edu/wpi/teamA/database/Interfaces/IEmployeeDAO.java
+++ src/main/java/edu/wpi/teamA/database/Interfaces/IEmployeeDAO.java
@@ -6,9 +6,10 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface IEmployeeDAO {
-  public HashMap<String, Employee> getAllEmployees();
 
   public Employee getEmployee(String name);
+
+  public HashMap<String, Employee> loadEmployeesFromDatabaseInMap();
 
   public void modifyEmployee(Employee employee);
 
