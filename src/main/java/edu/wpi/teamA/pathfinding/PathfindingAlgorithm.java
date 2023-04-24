f050c342b468f4a908b3933f1a731f4fcd1a8174
ashleigh <amperez@wpi.edu>
2023-04-23 21:57:33 -0400

    pathfinding functionality

--- src/main/java/edu/wpi/teamA/pathfinding/PathfindingAlgorithm.java
+++ src/main/java/edu/wpi/teamA/pathfinding/PathfindingAlgorithm.java
@@ -4,4 +4,18 @@ public enum PathfindingAlgorithm {
   ASTAR,
   BFS,
   DFS;
+
+  public String toString() {
+
+    switch (this) {
+      case ASTAR:
+        return "A*";
+      case BFS:
+        return "Breadth-First Search";
+      case DFS:
+        return "Depth-First Search";
+    }
+
+    return null;
+  }
 }
