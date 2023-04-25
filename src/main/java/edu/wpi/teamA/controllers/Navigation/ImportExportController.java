24b5de059409f636d0792876436dbdf59712e6dd
Seth Frank <sbfrank@wpi.edu>
2023-04-25 13:26:49 -0400

    Adding initial import export page

---
+++ src/main/java/edu/wpi/teamA/controllers/Navigation/ImportExportController.java
@@ -0,0 +1,98 @@
+package edu.wpi.teamA.controllers.Navigation;
+
+import javafx.fxml.FXML;
+
+public class ImportExportController {
+
+    @FXML
+    public void importNodes() {
+
+    }
+
+    @FXML public void importLocNames() {
+
+    }
+
+    @FXML public void importMoves() {
+
+    }
+
+    @FXML public void importEdges() {
+
+    }
+
+    @FXML public void importFlowers() {
+
+    }
+
+    @FXML public void importCRRR() {
+
+    }
+
+    @FXML public void importFurniture() {
+
+    }
+
+    @FXML public void importMeals() {
+
+    }
+
+    @FXML public void importEmployees() {
+
+    }
+
+    @FXML public void importUsers() {
+
+    }
+
+    @FXML public void importSignage() {
+
+    }
+
+
+
+    @FXML
+    public void exportNodes() {
+
+    }
+
+    @FXML public void exportLocNames() {
+
+    }
+
+    @FXML public void exportMoves() {
+
+    }
+
+    @FXML public void exportEdges() {
+
+    }
+
+    @FXML public void exportFlowers() {
+
+    }
+
+    @FXML public void exportCRRR() {
+
+    }
+
+    @FXML public void exportFurniture() {
+
+    }
+
+    @FXML public void exportMeals() {
+
+    }
+
+    @FXML public void exportEmployees() {
+
+    }
+
+    @FXML public void exportUsers() {
+
+    }
+
+    @FXML public void exportSignage() {
+
+    }
+}
