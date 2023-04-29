002fbf282f502f4a2cedd5a6de919eb87a89424c
ashleigh <amperez@wpi.edu>
2023-04-29 18:58:43 -0400

    merge

--- src/main/java/edu/wpi/teamA/controllers/Navigation/LocationSettingsController.java
+++ src/main/java/edu/wpi/teamA/controllers/Navigation/LocationSettingsController.java
@@ -1,6 +1,7 @@
 package edu.wpi.teamA.controllers.Navigation;
 
 import edu.wpi.teamA.App;
+import edu.wpi.teamA.database.DataBaseRepository;
 import edu.wpi.teamA.entities.MapEntity;
 import io.github.palexdev.materialfx.controls.MFXFilterComboBox;
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ public class LocationSettingsController {
   @FXML private MFXFilterComboBox locationComboBox;
   private MapEntity mapEntity = App.getMapEntity();
   private ArrayList<String> locationOptions;
+  private DataBaseRepository db = DataBaseRepository.getInstance();
 
   public void initialize() {
     // Getting LongNames from Database
@@ -26,8 +28,6 @@ public class LocationSettingsController {
   }
 
   public void setLocation() {
-    App.setCurrentLocation(
-        mapEntity.getLocationName(
-            mapEntity.getIDFromLongName(locationComboBox.getValue().toString())));
+    App.setCurrentLocation(db.getLocName(locationComboBox.getValue().toString()));
   }
 }
