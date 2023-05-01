a78f9a4ec1e97cad625e9fc9cc2b15b39a6d2a9b
Seth Frank <sbfrank@wpi.edu>
2023-05-01 17:24:31 -0400

    AWS works (and button reenabled to turn it on)

--- src/main/java/edu/wpi/teamA/database/DataBaseRepository.java
+++ src/main/java/edu/wpi/teamA/database/DataBaseRepository.java
@@ -466,15 +466,6 @@ public class DataBaseRepository {
       String firstName,
       String lastName,
       String userID) {
-    if (userName.length() < 3) {
-      System.out.println("username is too short");
-    } else if (password.length() < 5) {
-      System.out.println("password is too short, " + "must be more than 5 characters");
-    } else if (firstName.length() < 1) {
-      System.out.println("Please enter a first name");
-    } else if (lastName.length() < 1) {
-      System.out.println("please enter a last name");
-    }
     userDAOImp.addUser(adminYes, userName, password, firstName, lastName, userID);
   }
 
